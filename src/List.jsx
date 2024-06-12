aa22038e186b412d18cfe99d23a9a48eb8aac5bd
dalihzz <daliahdz123456@gmail.com>
2024-06-11 23:05:07 -0600

    first commit

---
+++ src/List.jsx
@@ -0,0 +1,42 @@
+import ListItem from "./ListItem"
+import PropTypes from "prop-types"
+
+const List = ({taskList, setTaskList}) => {
+
+return(
+    <div>
+    <div className="row m-2">
+        <div className="col-6">
+          <h4>Task</h4>
+        </div>
+        <div className="col-3">
+          <h4>Limit</h4>
+        </div>
+        <div className="col">
+          <h4>Location</h4>
+        </div>
+
+      </div>
+        {
+            taskList.map((task) =>{
+                return(
+                    <ListItem
+                    key={task.id}
+                    task={task}
+                    taskList={taskList}
+                    setTaskList={setTaskList}
+                    />
+                )
+            })
+        }
+    </div>
+    )
+}
+
+List.propTypes={
+  taskList:PropTypes.array.isRequired,
+  setTaskList:PropTypes.func.isRequired
+}
+
+
+export default List
